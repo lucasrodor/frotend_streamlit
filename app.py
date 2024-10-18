673d854a7ec144cccc7801805ac5f59be239ed5a
Lucas Rodor <lucasgomessr10@gmail.com>
2024-10-18 11:36:59 -0300

    initial commit

--- app.py
+++ app.py
@@ -2,45 +2,61 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import pathlib as Path
+from pathlib import Path
 
-caminho = Path(__file__).resolve().parent / "data" / "ibov.csv"
+# Definir o caminho do arquivo CSV
+caminho = Path(__file__).resolve().parent
 
 st.title('Meu primeiro dashboard')
 st.header('Esse é um header')
 
-if st.button('Clique aqui'):
-    st.text('Você apertou o botão')
+# Criar as abas do Streamlit
+abas = st.tabs(['Botão', 'Rádio', 'Dataframe', 'Gráfico'])
 
-opcao = st.radio(
-    'Escolha uma opção:',
-    ('Flamengo', 'Corinthians', 'Palmeiras')
-)
+# Aba 1 - Botão
+with abas[0]:
+    if st.button('Clique aqui'):
+        st.text('Você apertou o botão')
 
-if opcao == 'Flamengo':
-    st.info('Você é campeão!')
-elif opcao == 'Corinthians':
-    st.warning('Você é gambá!')
-elif opcao == 'Palmeiras':
-    st.error('Você é um perdedor!')
+# Aba 2 - Rádio
+with abas[1]:
+    opcao = st.radio(
+        'Escolha uma opção:',
+        ('Flamengo', 'Corinthians', 'Palmeiras')
+    )
 
-# Permitir upload de arquivo CSV
-uploaded_file = st.file_uploader("Escolha um arquivo CSV", type="csv")
+    if opcao == 'Flamengo':
+        st.info('Você é campeão!')
+    elif opcao == 'Corinthians':
+        st.warning('Você é gambá!')
+    elif opcao == 'Palmeiras':
+        st.error('Você é um perdedor!')
 
+# Aba 3 - DataFrame
+with abas[2]:
+    if caminho.exists():
+        # Carregar o arquivo CSV
+        df = pd.read_csv(str(caminho) + "/data/ibov.csv")
+        df_novo = df[['data', 'abertura', 'maximo', 'minimo', 'fechamento']]
+        st.dataframe(df_novo)
 
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    df_novo = df[['data', 'abertura', 'maximo', 'minimo', 'fechamento']]
-    
-    st.dataframe(df_novo)
-    
-    plt.style.use('_mpl-gallery')
-    
-    x = 0.5 + np.arange(8)
-    y = [4.8, 5.5, 3.5, 4.6, 6.5, 6.6, 2.6, 3.0]
-    
-    fig, ax = plt.subplots()
-    ax.bar(x, y, width=1, edgecolor='white', linewidth=0.7)
-    ax.set(xlim=(0, 8), xticks=np.arange(1, 8), ylim=(0, 8), yticks=np.arange(1, 8))
-    
-    st.pyplot(fig)
+# Aba 4 - Gráfico
+with abas[3]:
+    # Gráfico do preço de fechamento do IBOVESPA
+    st.title('Gráfico de Preço de Fechamento do IBOVESPA')
+
+    # Converter a coluna 'data' para o formato datetime
+    df_novo['data'] = pd.to_datetime(df_novo['data'])
+
+    # Criar o gráfico de linha
+    plt.figure(figsize=(10, 6))
+    plt.plot(df_novo['data'], df_novo['fechamento'], label='Fechamento', color='blue')
+
+    plt.title('Preço de Fechamento do IBOVESPA ao longo do tempo')
+    plt.xlabel('Data')
+    plt.ylabel('Preço de Fechamento (R$)')
+    plt.grid(True)
+    plt.legend()
+
+    # Exibir o gráfico no Streamlit
+    st.pyplot(plt)
